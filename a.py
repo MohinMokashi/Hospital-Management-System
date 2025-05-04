e474dab11fc5233448cc62b49250c44edb6456e0
MohinMokashi <mohinmokashi0313@gmail.com>
2025-05-04 23:10:36 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,28 @@
+print("hi")
+
+
+
+
+public void printInfo() {
+System.out.println(this.name);
+System.out.println(this.age);
+}
+}
+public class 00PS {
+Run | Debug
+public static void main(String args[]) {
+Student s1 = new Student();
+s1.name = "shradha";
+s1.age = 22;
+s1.printInfo();
+}
+}
+public class 00PS {
+Run | Debug
+public static void main(String args[]) {
+Pen pen1 = new Pen();
+pen1.color = "blue";
+pen1.type = "gel";
+Pen pen2 = new Pen();
+pen2.color = "black";
+pen2.type = "ballpoint";
---
+++ basic.js
@@ -0,0 +1,51 @@
+// console.log("hi");
+// console.table({t:"test"});
+// console.table({t:"test"});
+
+// let name = "swapnil";
+// console.log(name);
+// let score1 = 9;
+// console.log(score1);
+// let isLogin = false;
+// console.log(isLogin);
+// let teaTypes = ["s1","s2","22",2];
+// console.log(teaTypes);
+// let teaTypes1 = {s:"s1",h:"s2",j:"s3"};
+// console.log(teaTypes1);
+
+// let getScore = score1;
+// console.log(getScore);
+
+// let scoer2="swapnil";
+// scoer2="jagdish";  //score2 change as jagdish
+
+// const value= "deepak"; //it dosent change
+
+// let add=2+3
+//  let myscore =4
+//  myscore++  // myscore = 5
+
+//  let myscore1 =4
+//  myscore1--  // myscore = 4
+
+//  let sm1 = Symbol("he");
+//  let sm2= Symbol("he");
+//  console.log(sm1 == sm2);
+
+// const username= {
+//   firstname:"Swapnil",
+//   "last name":"Jadhav",
+//   isLoggedin:true,
+// };
+
+// username.firstname="jagdish";
+
+// console.log(username.firstname);
+// console.log(username["last name"]);
+// console.log(typeof username);
+
+let today = new Date();
+// console.log(today.getDate());
+
+let heros =["a","b","c",true,12];
+console.log(heros)
